8085e4828ad8fbe7523943d9f70a057fff5b1dbe
EC2 Default User <ec2-user@ip-172-31-38-184.ap-northeast-1.compute.internal>
2024-08-06 14:03:40 +0000

    リンク先修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,7 +25,7 @@ class ApplicationController < ActionController::Base
     when Customer
       about_path
     else
-      public_withdraw_customer_path
+      public_about_path
     end
   end
   
