eb6dd52aabf91f63b56031b438a8b7144d3ebc85
EC2 Default User <ec2-user@ip-172-31-38-184.ap-northeast-1.compute.internal>
2024-08-07 10:19:48 +0000

    テスト修正

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,55 +1,71 @@
 class Public::PostsController < ApplicationController
-    
-    def index
-      @posts = Post.order(created_at: :desc)  # 新規投稿順に並べ替え
-      
-    end
+  before_action :authenticate_customer!
+  before_action :correct_post, only: [:edit, :update]
 
-    def show
-      @post = Post.find(params[:id])
-    end
+  def index
+    @posts = Post.order(created_at: :desc)  # 新規投稿順に並べ替え
+  end
 
-    def new
-      @post = Post.new
-    end
-      # 投稿データの保存
-    def create
-      @post = Post.new(post_params)
-      @post.customer = current_customer # 現在の顧客を設定
-      if @post.save
-        flash[:notice] = '投稿成功しました'
-        redirect_to public_posts_path
-      else
-        flash.now[:alert] = '投稿失敗しました: ' + @post.errors.full_messages.join(", ")
-        render :new
-      end
-    end
-      
-    def edit
-      @post = Post.find(params[:id])
-    end
+  def show
+     @post = Post.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "指定された投稿が見つかりません。"
+    redirect_to public_posts_path
+  end
 
-    def update
-      @post = Post.find(params[:id])
-      if @post.update(post_params)
-        flash[:notice] = '投稿が更新されました'
-        redirect_to public_post_path(@post)
-      else
-        flash.now[:alert] = '投稿の更新に失敗しました: ' + @post.errors.full_messages.join(", ")
-        render :edit
-      end
-    end
+  def new
+    @post = Post.new
+  end
 
-    def destroy
-      @post = Post.find(params[:id])
-      @post.destroy
-      flash[:notice] = '投稿が削除されました'
+  # 投稿データの保存
+  def create
+    @post = Post.new(post_params)
+    @post.customer = current_customer # 現在の顧客を設定
+    if @post.save
+      flash[:notice] = '投稿成功しました'
+      redirect_to public_post_path(@post)
+    else
+      flash.now[:alert] = '投稿失敗しました: ' + @post.errors.full_messages.join(", ")
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:notice] = '投稿が更新されました'
+      redirect_to public_post_path(@post)
+    else
+      flash.now[:alert] = '投稿の更新に失敗しました: ' + @post.errors.full_messages.join(", ")
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:notice] = '投稿が削除されました'
+    redirect_to public_customer_path(current_customer)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content, :image)
+  end
+
+   def correct_post
+    @post = Post.find(params[:id])
+    unless @post.customer == current_customer
+      flash[:alert] = '権限がありません。'
       redirect_to public_posts_path
     end
-
-    private
-
-    def post_params
-      params.require(:post).permit(:content ,:image)
-    end
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "指定された投稿が見つかりません。"
+    redirect_to public_posts_path
+  end
 end
