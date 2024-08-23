103a4fb1e18fd0d145f0c4060b96b576d46d63ff
EC2 Default User <ec2-user@ip-172-31-38-184.ap-northeast-1.compute.internal>
2024-08-23 01:16:29 +0000

    save

--- spec/requests/public/posts_spec.rb
+++ spec/requests/public/posts_spec.rb
@@ -1,14 +1,40 @@
 #投稿機能に関するテスト
 require 'rails_helper'
+include Warden::Test::Helpers
 
-RSpec.describe "Posts", type: :request do
-  let(:customer) { FactoryBot.create(:customer) }
-  let(:valid_attributes) { { content: "新しい投稿", image: fixture_file_upload('test_image.png', 'image/png') } }
-  let(:invalid_attributes) { { content: "", image: nil } }
-
-  before do
-    sign_in customer
+RSpec.describe 'Posts', type: :request do
+  let(:customer) { create(:customer) }
+  let(:valid_attributes) do
+    {
+      content: 'This is a valid test post',
+      image: fixture_file_upload(Rails.root.join('spec/fixtures/files/test_image.png'), 'image/png')
+    }
   end
+
+  let!(:invalid_attributes) do
+    {
+      content: '',  # content を空にしてバリデーションエラーを発生させる
+      image: nil    # image を指定しないか、無効な値を設定
+    }
+  end
+  let(:post) { create(:post, customer: customer) }  # 適切なPostオブジェクトを定義
+  #ログイン時にする
+  # before do
+  #   post customer_session_path, params: { customer: { email: customer.email, password: customer.password } }
+  #   follow_redirect! # ログイン後のリダイレクトを追跡
+  # end
+  
+
+before do
+  Warden.test_mode!
+  login_as(customer, scope: :customer)
+end
+
+after do
+  Warden.test_reset!
+end
+  
+  
 
   describe "POST /create" do
     it "投稿詳細へ遷移すること" do
@@ -18,28 +44,29 @@ RSpec.describe "Posts", type: :request do
   end
 
   describe "POST /create with invalid data" do
-    it "バリデーションエラーメッセージが表示されること" do
+    it 'バリデーションエラーメッセージが表示されること' do
       post public_posts_path, params: { post: invalid_attributes }
-      document = Nokogiri::HTML(response.body)
-      expect(document.css('div#error_explanation').text).to include("Contentを入力してください")
-      expect(document.css('div#error_explanation').text).to include("Imageを選択してください")
+      expect(response.body).to include('投稿失敗しました') # バリデーションエラーメッセージを確認
     end
   end
+
 end
+
 
 
   describe "投稿編集" do
     context "編集ページへの遷移" do
       it "編集ページが表示されること" do
         get edit_public_post_path(post)
-        expect(response).to be_successful
+        expect(response).to have_http_status(:ok)
+        expect(response.body).to include('編集ページ') # 編集ページに含まれる内容を確認
       end
     end
-
+  end
     context "投稿を更新する場合" do
-      it "投稿詳細へ遷移すること" do
-        patch public_post_path(post), params: { post: { content: "更新された投稿" } }
-        expect(response).to redirect_to public_post_path(post)
+      it '投稿詳細へ遷移すること' do
+        post '/posts', params: { post: { content: 'Test content' } }
+        expect(response).to redirect_to(assigns(:post))
       end
     end
 
@@ -51,5 +78,4 @@ end
         expect(response.body).not_to include(post.content)
       end
     end
-  end
-end
+
