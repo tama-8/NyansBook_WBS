a5623602aefd214f5b6fb67032d57c95d4ba3def
EC2 Default User <ec2-user@ip-172-31-38-184.ap-northeast-1.compute.internal>
2024-08-28 11:30:15 +0000

    save

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :post do
     content { "This is a test post content" }
-    image { Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/files/test_image.png'), 'image/png') }
+    image { Rack::Test::UploadedFile.new(Rails.root.join("spec/fixtures/files/test_image.png"), "image/png") }
     association :customer # ポストが顧客に紐づいている場合
   end
 end
--- spec/views/admin/dashboard/index.html.erb_spec.rb
+++ spec/views/admin/dashboard/index.html.erb_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "admin/dashboard/index", type: :view do
   context "管理者がログインしている場合" do
@@ -22,11 +22,11 @@ RSpec.describe "admin/dashboard/index", type: :view do
     end
 
     it "管理者向けのナビゲーションリンクが表示されることを確認する" do
-      expect(rendered).to have_link('ダッシュボード', href: admin_dashboard_path)
-      expect(rendered).to have_link('Customer List', href: admin_root_path)
-      expect(rendered).to have_link('Post List', href: admin_posts_path)
-      expect(rendered).to have_link('Comment List', href: admin_post_comments_path)
-      expect(rendered).to have_link('logout', href: destroy_admin_session_path)
+      expect(rendered).to have_link("ダッシュボード", href: admin_dashboard_path)
+      expect(rendered).to have_link("Customer List", href: admin_root_path)
+      expect(rendered).to have_link("Post List", href: admin_posts_path)
+      expect(rendered).to have_link("Comment List", href: admin_post_comments_path)
+      expect(rendered).to have_link("logout", href: destroy_admin_session_path)
     end
   end
 
@@ -35,7 +35,7 @@ RSpec.describe "admin/dashboard/index", type: :view do
       # 管理者のログイン状態がfalseであることをシミュレート
       allow(view).to receive(:admin_signed_in?).and_return(false)
       allow(view).to receive(:customer_signed_in?).and_return(false)
-        # Devise関連のリソースを設定
+      # Devise関連のリソースを設定
       view.define_singleton_method(:resource) { Customer.new }
       view.define_singleton_method(:resource_name) { :customer }
       view.define_singleton_method(:devise_mapping) { Devise.mappings[:customer] }
@@ -44,13 +44,13 @@ RSpec.describe "admin/dashboard/index", type: :view do
     end
 
     it "ログインページにゲスト向けのナビゲーションリンクが表示されることを確認する" do
-      expect(rendered).to have_link('Home', href: root_path)
-      expect(rendered).to have_link('About', href: public_about_path)
-      expect(rendered).to have_link('sign up', href: new_customer_registration_path)
-      expect(rendered).to have_link('login', href: new_customer_session_path)
-      expect(rendered).not_to have_link('MyHome')
-      expect(rendered).not_to have_link('MyPage')
-      expect(rendered).not_to have_link('logout')
+      expect(rendered).to have_link("Home", href: root_path)
+      expect(rendered).to have_link("About", href: public_about_path)
+      expect(rendered).to have_link("sign up", href: new_customer_registration_path)
+      expect(rendered).to have_link("login", href: new_customer_session_path)
+      expect(rendered).not_to have_link("MyHome")
+      expect(rendered).not_to have_link("MyPage")
+      expect(rendered).not_to have_link("logout")
     end
   end
 end
