088e9938a04b472ef2407208d618f3fc101d6dca
EC2 Default User <ec2-user@ip-172-31-38-184.ap-northeast-1.compute.internal>
2024-09-10 14:07:38 +0000

    spec編集

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,6 +1,12 @@
 FactoryBot.define do
-  factory :post do
+  factory :valid_post, class: Post do
     content { "This is a test post content" }
+    image { Rack::Test::UploadedFile.new(Rails.root.join("spec/fixtures/files/test_image.png"), "image/png") }
+    association :customer # ポストが顧客に紐づいている場合
+  end
+  
+  factory :invalid_post, class: Post do
+    content { "" }
     image { Rack::Test::UploadedFile.new(Rails.root.join("spec/fixtures/files/test_image.png"), "image/png") }
     association :customer # ポストが顧客に紐づいている場合
   end
--- spec/requests/public/posts_spec.rb
+++ spec/requests/public/posts_spec.rb
@@ -18,60 +18,119 @@ RSpec.describe "Posts", type: :request do
       image: nil    # image を指定しないか、無効な値を設定
     }
   end
-   let(:post) { create(:post, customer: customer) }  # 適切なPostオブジェクトを定義
+  #let(:post) { create(:post, customer: customer) }  # 適切なPostオブジェクトを定義
   # ログイン時にする
   # before do
   #   post customer_session_path, params: { customer: { email: customer.email, password: customer.password } }
   #   follow_redirect! # ログイン後のリダイレクトを追跡
   # end
-
-
+  
   before do
-    Warden.test_mode!
-    login_as(customer, scope: :customer)
+    #Warden.test_mode!
+    #login_as(customer, scope: :customer)
   end
 
   after do
     Warden.test_reset!
   end
+  
+  describe "#index" do
+    context "ログインしたcustomerの場合" do
+      before do
+        Warden.test_mode!
+        login_as(customer, scope: :customer)
+      end
+      it "200を返すこと" do
+        get public_posts_url
+        expect(response).to be_successful
+        expect(response).to have_http_status "200"
+      end
+    end
+    
+    context "未ログインの場合" do
+      it "root_urlにリダイレクトされること" do
+        get public_posts_url
+        expect(response).to have_http_status "302"
+        expect(response).to redirect_to new_customer_session_url
+      end
+    end
+  end
+  
+  describe "#create" do
+    context "ログインしたcustomerの場合" do
+      before do
+        Warden.test_mode!
+        login_as(customer, scope: :customer)
+      end
+      
+      it "createが成功したときshowにリダイレクトされること" do
+        valid_attributes = FactoryBot.attributes_for(:valid_post)
+        expect {
+          post public_posts_url, params: { post: valid_attributes }
+        }.to change(Post, :count).by(1)
+        expect(flash[:notice]).to include "投稿成功しました"
+        expect(response).to have_http_status "302"
+        expect(response).to redirect_to public_post_url(Post.last)
+      end
+      
+      it "createが失敗したときリダイレクトしないこと" do
+        invalid_attributes = FactoryBot.attributes_for(:invalid_post)
+        expect {
+          post public_posts_url, params: { post: invalid_attributes }
+        }.not_to change(Post, :count)
+        expect(flash.now[:alert]).to include "投稿失敗しました: "
+        expect(response).to have_http_status "200"
+      end        
+    end
+    context "未ログインの場合" do
+      it "root_urlにリダイレクトされること" do
+        post public_posts_url
+        expect(response).to have_http_status "302"
+        expect(response).to redirect_to root_url
+      end
+    end
+  end
 
-describe '投稿一覧画面のテスト' do
+  describe '投稿一覧画面のテスト' do
     before do
       
     # visit  new_customer_session_path
     # fill_in 'customer[email]', with: customer.email
     # fill_in 'customer[password]', with: customer.password
     # click_button 'Log in'
- 
-      visit new_public_post_path
+      #byebug
+      #visit new_public_post_path
     end
- context '投稿成功のテスト' do
+    context '投稿成功のテスト' do
       before do
-        fill_in 'post[content]', with: Faker::Lorem.characters(number: 5)
+        
+        #fill_in 'post[content]', with: Faker::Lorem.characters(number: 5)
         # fill_in 'book[body]', with: Faker::Lorem.characters(number: 20)
       end
 
-  describe "POST /create" do
-    # it "投稿詳細へ遷移すること" do
+      describe "POST /create" do
+      # it "投稿詳細へ遷移すること" do
       # self.post public_posts_path, params: { post: valid_attributes }
-      it 'リダイレクト先が、保存できた投稿の詳細画面になっている', spec_category: "CRUD機能に対するコントローラの処理と流れ(ログイン状況を意識した応用)" do
-        click_button '投稿'
-        expect(current_path).to eq '/public/posts/' + Post.last.id.to_s
-        # expect(current_path).to eq '/public/posts/new'
-        # is_expected.to eq '/public/posts/' + post.id.to_s
-       
+        it 'リダイレクト先が、保存できた投稿の詳細画面になっている', spec_category: "CRUD機能に対するコントローラの処理と流れ(ログイン状況を意識した応用)" do
+          visit new_public_post_path
+          fill_in 'post[content]', with: Faker::Lorem.characters(number: 5)
+          click_button '投稿'
+          expect(current_path).to eq '/public/posts/' + Post.last.id.to_s
+            # expect(current_path).to eq '/public/posts/new'
+            # is_expected.to eq '/public/posts/' + post.id.to_s
+           
+        end
+          # expect(response).to redirect_to(public_posts_path(Post.last))
+          
+        # end
       end
-      # expect(response).to redirect_to(public_posts_path(Post.last))
-      
-    # end
-  end
-end
-  describe "POST /create with invalid data" do
-    it "バリデーションエラーメッセージが表示されること" do
-      post public_posts_path, params: { post: invalid_attributes }
-      expect(response.body).to include("投稿失敗しました") # バリデーションエラーメッセージを確認
     end
-  end
+    describe "POST /create with invalid data" do
+      it "バリデーションエラーメッセージが表示されること" do
+        post public_posts_path, params: { post: invalid_attributes }
+        expect(response.body).to include("投稿失敗しました") # バリデーションエラーメッセージを確認
+      end
+    end
 end
 
 
